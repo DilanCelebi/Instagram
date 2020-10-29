2b8a7576e8cd0407fe644b9c664ebee4b5138f23
Dilan Çelebi <dilancelebi08@gmail.com>
2020-10-29 13:04:35 +0300

    First Commit

---
+++ Instagram/Controllers/Core Tabs/CameraViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CameraViewController.swift
+//  Instagram
+//
+//  Created by Dilan Celebi on 29.10.2020.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ Instagram/Controllers/Core Tabs/ExploreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExploreViewController.swift
+//  Instagram
+//
+//  Created by Dilan Celebi on 29.10.2020.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
